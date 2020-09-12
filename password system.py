d0896cc0561c3935573d998250819edfa1722ceb
GamingSpruce <69159094+GamingSpruce@users.noreply.github.com>
2020-09-12 11:37:37 +0100

    Create password system.py

---
+++ password system.py
@@ -0,0 +1,11 @@
+password = ''
+
+while password != 'password':
+    print("what is the password?")
+    password = input()
+    if password == "password":
+        print("Correct")
+    else:
+        print("Incorrect try again")
+    
+
